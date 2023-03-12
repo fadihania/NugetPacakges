a55b8dfb1dcd22f024f804f9f399b7a733a37989
Fadi Hania <fhania@gmail.com>
2023-03-12 21:24:35 +0200

    Sample code from package website to resize a sample image

--- NugetPacakges/Program.cs
+++ NugetPacakges/Program.cs
@@ -1,2 +1,6 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+using var image = Image.Load(@"..\..\..\food.jpg");
+image.Mutate(x => x.Resize(image.Width / 2, image.Height / 2));
+image.Save(@"..\..\..\result.jpg");
