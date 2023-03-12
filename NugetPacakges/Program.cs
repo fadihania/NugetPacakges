96391ffffc8ddb900e36d9f4c1432e07ce301b06
Fadi Hania <fhania@gmail.com>
2023-03-12 21:29:32 +0200

    Using await with async methods for better IO handling

--- NugetPacakges/Program.cs
+++ NugetPacakges/Program.cs
@@ -1,6 +1,7 @@
-﻿using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Processing;
+﻿
 
 using var image = Image.Load(@"..\..\..\food.jpg");
+
 image.Mutate(x => x.Resize(image.Width / 2, image.Height / 2));
-image.Save(@"..\..\..\result.jpg");
+
+await image.SaveAsync(@"..\..\..\result.jpg");
